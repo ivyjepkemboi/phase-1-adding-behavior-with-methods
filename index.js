a0ab0d04b619f2d11070800bfc6699b87748a621
Ivy <ivyjkemboi@gmail.com>
2024-04-30 22:12:42 +0300

    final

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+// Define the Cat class
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  // Define the Dog class
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  // Define the Bird class
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      if (this.sex === 'male') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+
+  
